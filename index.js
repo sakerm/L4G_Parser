f9fcab5adc8eeaee02eeea5350283677dfc03af5
MERESSE <Lorenzo.MERESSE@sage.com>
2020-03-30 14:06:04 +0200

    move to bin

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var parser_1 = require("./lib/parser");
 var fs = require("fs");
 var str = [];
@@ -7,7 +7,7 @@ var copy = [];
 var count = 0;
 var value;
 var file;
-fs.readFile('./file/test.src', 'utf8', function (err, files) {
+fs.readFile('./file/ASYRADELETE.src', 'utf8', function (err, files) {
     file = files.split('\n');
     while (file.length != count) {
         value = new parser_1.Parser(file[count]).parse();
--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("salut")
+import { Parser } from './lib/parser'
+import * as fs from 'fs'
+
+let str: string[] = [];
+let copy: string[] = [];
+let count: number = 0
+let value
+let file;
+
+fs.readFile('./file/ASYRADELETE.src', 'utf8', function (err, files) {
+  file = files.split('\n')
+  while (file.length != count) {
+    value = new Parser(file[count]).parse();
+    console.log(JSON.stringify(value))
+    count += 1
+  };
+})
