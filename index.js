63588deea5becbd2b6063dd2bff4bfe27f0c013f
MERESSE <Lorenzo.MERESSE@sage.com>
2020-04-14 14:16:06 +0200

    argOk

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var parser_1 = require("../parser");
+var parser_1 = require("./lib/parser");
 var fs = require("fs");
 var str;
-fs.readFileSync('./file/ASYRADELETE.src', 'utf8').toString().split('\n').forEach(function (line) {
+fs.readFileSync(process.argv[2], 'utf8').toString().split('\n').forEach(function (line) {
     // if (line.indexOf('#') == -1) {
     str += line;
     // }
--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
-import { Parser } from '../parser'
+import { Parser } from './lib/parser'
 import * as fs from 'fs'
 
 let str: any;
 
-fs.readFileSync('./file/ASYRADELETE.src', 'utf8').toString().split('\n').forEach(function (line) {
+fs.readFileSync(process.argv[2], 'utf8').toString().split('\n').forEach(function (line) {
    // if (line.indexOf('#') == -1) {
         str += line;
    // }
@@ -13,7 +13,6 @@ console.log(JSON.stringify(str))
 let value
 value = new Parser(str).parse();
 console.log(JSON.stringify(value))
-
 //console.log(JSON.stringify(str))
 
 //const parsed = new Parser('NBRROL += 1').parse();
