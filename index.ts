b90cea4a0c896085176421505c044a77de2ff740
Lorenzo Meresse <lorenzomeresse@Lorenzos-MacBook-Pro.local>
2020-05-18 13:54:18 +0200

    tslintIsOk

--- index.ts
+++ index.ts
@@ -9,15 +9,15 @@ function readFiles(dirname) {
        return;
      }
      filenames.forEach(function(filename) {
-      fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+      fs.readFile(dirname + filename, 'utf-8', function() {
         if (err) {
           return;
         }
         fs.readFileSync(dirname+filename, 'utf8').toString().split('\n').forEach(function (line) {
                str += line;
          });
-         console.log(str + '\n' + "-------------------"+filename +"-----------------------")
-         str = '\0'
+         console.log(str + '\n' + '-------------------'+filename +'-----------------------');
+         str = '\0';
       });
     });
   });
@@ -34,7 +34,7 @@ function readFile(filename) {
 
 function is_dir(path) {
    try {
-       var stat = fs.lstatSync(path);
+       const stat = fs.lstatSync(path);
        return stat.isDirectory();
    } catch (e) {
        // lstatSync throws an error if path doesn't exist
@@ -42,17 +42,15 @@ function is_dir(path) {
    }
 }
 
-if (is_dir(process.argv[2]) == true) {
+if (is_dir(process.argv[2])) {
    readFiles(process.argv[2]);
 }
 else {
-   str = readFile(process.argv[2])
-   console.log(JSON.stringify(str));
+   console.log(JSON.stringify(readFile(process.argv[2])));
    let value: any;
-   value = new Parser(str).parse();
+   value = new Parser(readFile(process.argv[2])).parse();
    console.log(JSON.stringify(value));
 }
-
 
 //console.log(JSON.stringify(str))
 
