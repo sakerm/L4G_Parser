6b7f9d654d14a80e3d51ffbffcdd49799f55bf94
Lorenzo Meresse <lorenzomeresse@Lorenzos-MacBook-Pro.local>
2020-05-18 13:46:28 +0200

    filesDirectoryCanBeRead

--- index.ts
+++ index.ts
@@ -3,17 +3,57 @@ import { Parser } from './lib/parser';
 
 let str: any;
 
-console.log(process.argv);
-fs.readFileSync(process.argv[2], 'utf8').toString().split('\n').forEach(function (line) {
-   // if (line.indexOf('#') == -1) {
-        str += line;
-   // }
-});
+function readFiles(dirname) {
+   fs.readdir(dirname, function(err, filenames) {
+     if (err) {
+       return;
+     }
+     filenames.forEach(function(filename) {
+      fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+        if (err) {
+          return;
+        }
+        fs.readFileSync(dirname+filename, 'utf8').toString().split('\n').forEach(function (line) {
+               str += line;
+         });
+         console.log(str + '\n' + "-------------------"+filename +"-----------------------")
+         str = '\0'
+      });
+    });
+  });
+}
 
-console.log(JSON.stringify(str));
-let value: any;
-value = new Parser(str).parse();
-console.log(JSON.stringify(value));
+function readFile(filename) {
+   fs.readFileSync(filename, 'utf8').toString().split('\n').forEach(function (line) {
+      // if (line.indexOf('#') == -1) {
+           str += line;
+      // }
+   });
+   return(str);
+}
+
+function is_dir(path) {
+   try {
+       var stat = fs.lstatSync(path);
+       return stat.isDirectory();
+   } catch (e) {
+       // lstatSync throws an error if path doesn't exist
+       return false;
+   }
+}
+
+if (is_dir(process.argv[2]) == true) {
+   readFiles(process.argv[2]);
+}
+else {
+   str = readFile(process.argv[2])
+   console.log(JSON.stringify(str));
+   let value: any;
+   value = new Parser(str).parse();
+   console.log(JSON.stringify(value));
+}
+
+
 //console.log(JSON.stringify(str))
 
 //const parsed = new Parser('NBRROL += 1').parse();
