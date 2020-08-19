ee8b4c70c27dcc9d90d1d2df0f72f22296c98d6a
sakerm <michelangelilorenzo@gmail.com>
2020-08-19 17:25:27 +0200

    add

---
+++ lib/checkparse.ts
@@ -0,0 +1,10 @@
+export class check_parameter {
+    Param: () => {
+        JSONParam: string;
+    };
+};
+function unusedParameter(JSONParam: string) {
+    console.log("---------------------------------------------------")
+    
+    console.log(JSONParam);
+}
